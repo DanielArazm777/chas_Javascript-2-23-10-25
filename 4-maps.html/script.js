2038e80b7a33bdc197a56a0be65c120a2cbb2b7c
Daniel Arazm <Daniel.Arazm@chasacademy.se>
2025-10-23 19:26:53 +0200

    klar med uppgift 4

---
+++ 4-maps.html/script.js
@@ -0,0 +1,11 @@
+
+  const dogs = ["Labrador", "Beagle", "Poodle", "Pug", "Chihuahua"]
+
+
+    const doggieWalks = dogs.maps((dogName) => { //ändrade foreach till map//
+    return "The " + dogName + " needs to go for a walk!"
+  });
+
+const shoutyDogNames = dogs.maps(dogName => dogName.toUpperCase) => { //ändrade så det blir uppercase  sen skriver vi ut det med console.log //
+    console.log(shoutyDogNames);
+}
